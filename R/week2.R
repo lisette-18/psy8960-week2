19d770e1295f76151f026f13540d744264e3e5f7
lisette-18 <horne146@umn.edu>
2024-01-28 12:23:23 -0600

    Change genders to reflect male, female, and nonbinary

--- R/week2.R
+++ R/week2.R
@@ -1,3 +1,4 @@
 #Import and Labeling
 rt_df <- read.csv("data/week2.csv", header = TRUE)
 rt_df$condition <- factor(rt_df$condition, ordered = TRUE, levels = c("A", "B"), labels = c("Control", "Experimental"))
+rt_df$gender <- factor(rt_df$gender, ordered = TRUE, levels = c("M", "F", "N"), labels = c("Male", "Female", "Nonbinary"))
