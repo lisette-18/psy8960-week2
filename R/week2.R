6a17588c5d96d6f283c38bddf009a2ea37457019
lisette-18 <horne146@umn.edu>
2024-01-28 12:25:59 -0600

    Mean and new data frame with only females

--- R/week2.R
+++ R/week2.R
@@ -2,3 +2,7 @@
 rt_df <- read.csv("data/week2.csv", header = TRUE)
 rt_df$condition <- factor(rt_df$condition, ordered = TRUE, levels = c("A", "B"), labels = c("Control", "Experimental"))
 rt_df$gender <- factor(rt_df$gender, ordered = TRUE, levels = c("M", "F", "N"), labels = c("Male", "Female", "Nonbinary"))
+
+#Analysis
+mean(rt_df$rt)
+rt_f_df <- subset(rt_df, subset = gender == "Female")
