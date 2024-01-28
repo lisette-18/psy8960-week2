cfc03e1a43b25d70359f7e0e11294be66b403dde
lisette-18 <horne146@umn.edu>
2024-01-28 12:20:27 -0600

    Change condition to reflect control and experimental

--- R/week2.R
+++ R/week2.R
@@ -1,2 +1,3 @@
 #Import and Labeling
 rt_df <- read.csv("data/week2.csv", header = TRUE)
+rt_df$condition <- factor(rt_df$condition, ordered = TRUE, levels = c("A", "B"), labels = c("Control", "Experimental"))
